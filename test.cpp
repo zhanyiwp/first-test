5989eb65b85e68884092c7c69350bf28fbd8816a
zhanyiwp <zhanyiwp@163.com>
2016-07-13 15:51:52 +0800

    first test
    1111

---
+++ test.cpp
@@ -0,0 +1,20 @@
+#include "callback.h"
+typedef void (*CallBackFun)(void *watcherCtx); 
+
+void callback(void *watcherCtx)
+{
+	call_back *p = (call_back *)watcherCtx;
+	p->call();
+}
+
+int test(CallBackFun pCallBack, void *watcherCtx)
+{
+	pCallBack(watcherCtx);
+	return 0;
+}
+int main()
+{
+		calltest A;
+		A.Run();
+		test(callback,&A.m_test);
+}
